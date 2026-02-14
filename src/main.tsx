57a43b90208e788818007857b92f973fa7341c7d
oliver-nap <oliverhoeber006@gmail.com>
2026-02-14 18:27:17 +0100

    mobile: disable zoom and prevent scroll

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+document.addEventListener("gesturestart", (e) => e.preventDefault());
+document.addEventListener("gesturechange", (e) => e.preventDefault());
+document.addEventListener("gestureend", (e) => e.preventDefault());
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
